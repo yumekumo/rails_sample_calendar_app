d77840e89fd0850a1b4310d97af1a89082f16c33
yumekumo <chihachiha.ryotakun@gmail.com>
2021-09-09 08:10:01 +0000

    [Add] Indexビューの追加

---
+++ db/migrate/20210909064825_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :blogs do |t|
+
+      t.string :title
+      t.text :content
+      t.datetime :start_time
+      
+      t.timestamps
+    end
+  end
+end
